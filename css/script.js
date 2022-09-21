430b98fbc8ab323b0d9ee3d160d680755e44017a
Abdousmane98 <abdousmane98@gmail.com>
2022-09-21 19:08:03 +0200

    new

--- css/script.js
+++ css/script.js
@@ -1,8 +1,10 @@
-function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+
+  function personne (nom, prenom, age, sexe, adresse) {
+    this.nom= nom
+    this.prenom= prenom
+    this.age= age
+    this.sexe= sexe
+    this.adresse=  adresse
+}
+let personne= new personne('abdou', 'ousmane', 24, 'Masculin', 'welsch')
+console.log ( personne.nom, personne.prenom, personne.age, personne.sexe, personne.adresse )
